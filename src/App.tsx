d6e8562bd28cf3d5bc2a22aabcbaf349c816846b
ShadowWalker2014 <kai.jiabo.feng@gmail.com>
2025-03-04 03:15:44 +0000

    Fix Server Issues and Dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './components/ThemeProvider'
 import { HomePage } from './pages/HomePage'
-import { DocsPage } from './pages/DocsPage'
-import { DocPage } from './pages/DocPage'
-import { LoginPage } from './pages/LoginPage'
-import { DashboardPage } from './pages/cms/DashboardPage'
-import { DocumentEditorPage } from './pages/cms/DocumentEditorPage'
 
 function App() {
   return (
@@ -13,13 +8,6 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/docs" element={<DocsPage />} />
-          <Route path="/docs/category/:categoryId" element={<DocsPage />} />
-          <Route path="/docs/:slug" element={<DocPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/cms" element={<DashboardPage />} />
-          <Route path="/cms/documents/new" element={<DocumentEditorPage />} />
-          <Route path="/cms/documents/edit/:id" element={<DocumentEditorPage />} />
         </Routes>
       </Router>
     </ThemeProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
-import rehypeHighlight from 'rehype-highlight'
-import rehypeSlug from 'rehype-slug'
-import remarkGfm from 'remark-gfm'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    mdx({
-      rehypePlugins: [rehypeHighlight, rehypeSlug],
-      remarkPlugins: [remarkGfm],
-    }),
-  ],
+  plugins: [react()],
+  server: {
+    port: 3000,
+    host: true
+  }
 })
